6110f2c8e44b816368ea8a833ddea602a56fe2f9
sonewang168 <anony17898@gmail.com>
2026-01-15 20:34:32 +0800

    fix: SW v3 強制刷新

--- client/sw.js
+++ client/sw.js
@@ -1,36 +1,23 @@
-// Service Worker for 股海秘書 PWA
-const CACHE_NAME = 'stock-secretary-v1';
-const urlsToCache = [
-  '/',
-  '/manifest.json',
-  '/images/icon-192.png',
-  '/images/icon-512.png'
-];
+﻿const CACHE_NAME = 'stock-secretary-v3';
+const urlsToCache = ['/', '/manifest.json', '/images/icon-192.png', '/images/icon-512.png'];
 
-// 安裝
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-      .catch(function(err) {
-        console.log('Cache error:', err);
-      })
-  );
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-// 啟動
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== CACHE_NAME;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
         })
       );
     })
@@ -38,16 +25,10 @@ self.addEventListener('activate', function(event) {
   self.clients.claim();
 });
 
-// 攔截請求
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
   );
 });
